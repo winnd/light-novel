e8f1bcbd4dcf901e9fc4e5f759ebd57454a4b538
Fengye.Ren <AS40879@yeah.net>
2018-12-28 17:39:32 +0800

    新增聊天室

---
+++ src/app/page-construction/chart-room/chart-room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-room',
+  templateUrl: './chart-room.component.html',
+  styleUrls: ['./chart-room.component.less']
+})
+export class ChartRoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
